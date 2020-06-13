cd87812056f07b88a7cd047eb03cf93d11151b77
apple-yagi <applesryu@gmail.com>
2020-06-13 23:32:51 +0900

    index.vueを追加

---
+++ components/index/IndexTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 align="center">
+    <client-only>
+      <vue-typer
+        text="Hello World!"
+        erase-style="backspace"
+        :erase-on-complete="true"
+        :pre-erase-delay="300"
+        :erase-delay="150"
+        :repeat="0"
+      ></vue-typer>
+      <vue-typer
+        text="Welcome to My portfolio page!"
+        :repeat="0"
+        :pre-type-delay="3500"
+      ></vue-typer>
+    </client-only>
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexTitle'
+})
+</script>
---
+++ components/index/YmlFile.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-layout justify-center>
+      <v-col cols="12" sm="10" lg="8">
+        <v-sheet>
+          <span class="body-1" style="background-color: #555;">profile.yml</span>
+          <v-container>
+            <client-only>
+              <div v-for="(code, i) in codes" :key="i">
+                <vue-typer :text="code.text" :repeat="0" :pre-type-delay="code.delay"></vue-typer>
+              </div>
+            </client-only>
+          </v-container>
+        </v-sheet>
+      </v-col>
+    </v-layout>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'YmlFile',
+
+  data: () => ({
+    codes: [
+      { text: 'version : ' + new Date().getFullYear(), delay: 5800 },
+      { text: 'jobs :', delay: 7000 },
+      { text: '  -  4th year University Student', delay: 7800 },
+      { text: 'live :', delay: 10300 },
+      { text: '  -  Fukuoka', delay: 11100 },
+      { text: 'favorite lang :', delay: 12000 },
+      { text: '  -  JavaScript, Node.js', delay: 13000 }
+    ]
+  })
+})
+</script>
